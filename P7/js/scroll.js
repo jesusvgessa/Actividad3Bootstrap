8afc52cebd72f61029fa9a77870892b690013a69
jesusvgessa <jesusvgessa@hotmail.com>
2022-02-05 13:45:51 +0100

    p7

--- P7/js/scroll.js
+++ P7/js/scroll.js
@@ -1,3 +1,19 @@
+var scrollSpy = new bootstrap.ScrollSpy(document.body, {
+    target: '#navbar-example'
+})
+
+var dataSpyList = Array.prototype.slice.call(document.querySelectorAll('[data-bs-spy="scroll"]'))
+dataSpyList.forEach(function(dataSpyEl) {
+    bootstrap.ScrollSpy.getInstance(dataSpyEl)
+        .refresh()
+})
+
+var scrollSpyContentEl = document.getElementById('content')
+var scrollSpy = bootstrap.ScrollSpy.getInstance(scrollSpyContentEl) // Returns a Bootstrap scrollspy instance
+
+var scrollSpyContentEl = document.getElementById('content')
+var scrollSpy = bootstrap.ScrollSpy.getOrCreateInstance(scrollSpyContentEl) // Returns a Bootstrap scrollspy instance
+
 var firstScrollSpyEl = document.querySelector('[data-bs-spy="scroll"]')
 firstScrollSpyEl.addEventListener('activate.bs.scrollspy', function() {
     // do something...
